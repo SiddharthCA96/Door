fc067c4a9a11092bdeffb65b8d17469a6af03e03
SiddharthCA96 <ss6156852@gmail.com>
2025-01-27 10:01:19 +0530

    backend setup done and routes created

---
+++ backend/src/index.ts
@@ -0,0 +1,32 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+//signin route
+app.post("/api/v1/signin", (c) => {
+  return c.text("signin route");
+});
+
+//sign up route
+app.post("/api/v1/signup", (c) => {
+  return c.text("signup route");
+});
+
+//blog post route
+app.post("/api/v1/blog", (c) => {
+  return c.text("blog created route");
+});
+//blog update
+app.post("api/v1/blog", (c) => {
+  return c.text("blog update route");
+});
+//route to get a blog
+app.get("/api/v1/blog/:id", (c) => {
+  const id = c.req.param("id");
+  console.log(id);
+  return c.text("get blog route");
+});
+export default app;
