32f58617d166b507791e3a0f25a4f9f7d9796b3e
SiddharthCA96 <ss6156852@gmail.com>
2025-01-31 10:44:47 +0530

    signup and sign in route done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,72 @@
 import { Hono } from "hono";
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
 
-const app = new Hono();
+//pass the types of env as a generic (this is the way provided by hono to give the types of environment variables)
+const app = new Hono<{
+  //telling the types of our db url
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 //signin route
-app.post("/api/v1/signin", (c) => {
-  return c.text("signin route");
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  //get the request body from the context
+  const body = await c.req.json();
+
+  //create a new user
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    //create the jwt token and return it to user
+    const jwt = await sign({ id: user.id }, c.env?.JWT_SECRET);
+    return c.json({ jwt });
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "error while signing up" });
+  }
 });
 
 //sign up route
-app.post("/api/v1/signup", (c) => {
-  return c.text("signup route");
-});
+app.post("/api/v1/signin", async(c) => {
+  //initialize prisma
+  const prisma=new PrismaClient({
+    datasourceUrl:c.env?.DATABASE_URL,    
+  }).$extends(withAccelerate());
+  //get the body
+  const body=await c.req.json();
 
+  //find the user 
+  const user=await prisma.user.findUnique({
+    where:{
+      email:body.email,
+      password:body.password
+    }
+  });
+  if(!user){
+    c.status(403);
+    return c.json({error:"User not found"});
+  };
+   
+  //returnn the jwt to user
+  const jwt=await sign({id:user.id},c.env.JWT_SECRET);
+  return c.json({jwt});
+});
 
 //blog post route
 app.post("/api/v1/blog", (c) => {
