0268d1a3cb8bfb54154982e8b16ca44d52f1cbf8
SiddharthCA96 <ss6156852@gmail.com>
2025-02-02 17:45:01 +0530

    sdsd

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { sign } from "hono/jwt";
+import {signupInput} from "@siddharthsingh/door-common"
 
 
 //pass the types of env as a generic (this is the way provided by hono to give the types of environment variables)
